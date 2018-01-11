265f35432f7e8b0660a01c3849af681e0e0909aa
Hezekiah Branch <hlbranch@icloud.com>
2018-01-11 21:33:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // write your code below!
+function happyHolidays(string) {
+  return ("Happy holidays!")
+}
+
+function happyHolidaysTo(string) {
+  var name = 'you'
+  if (name === "you") {
+    return (`Happy holidays, ${name}!`)
+  }
+}
+
+function happyHolidayTo(holiday, name) {
+  var holiday = 'Independence Day'
+  var name = 'you'
+  return (`Happy ${holiday}, ${name}!`)
+}
+
+function holidayCountdown(holiday, days) {
+  var holiday = "Mother's Day"
+  var days = 20
+  return (`It's ${days} days until ${holiday}!`)
+}
